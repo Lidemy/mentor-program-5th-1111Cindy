bcd7bc30fb8b56e7df988e2611131aa1fef1e583
Cindy <cindy821111@gmail.com>
2021-04-25 21:44:40 +0900

    finished hw2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,11 @@
 function capitalize(str) {
-  
+	if(str[0] >= "A" && str[0] <= "Z" || str[0] >= "a" && str[0] <= "z"){
+		var strBig = str.replace(str[0],str[0].toUpperCase())
+		return strBig
+	}
+	return str
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize('nick'))
+console.log(capitalize('Nick'))
+console.log(capitalize(',hello'))
